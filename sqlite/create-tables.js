abc6fafcb2f865e028bf3a215202f69f47940cc9
Shivam Shahi <shivam.s@coinswitch.co>
2022-07-29 01:00:12 +0530

    add categories fk

--- sqlite/create-tables.js
+++ sqlite/create-tables.js
@@ -1,9 +1,14 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 // open the database connection
-let db = new sqlite3.Database('sample.db');
+let db = new sqlite3.Database("sample.db");
+db.get("PRAGMA foreign_keys = ON");
 
-db.run('CREATE TABLE categories(name text, id INTEGER PRIMARY KEY AUTOINCREMENT)');
-db.run('CREATE TABLE books(ID INTEGER PRIMARY KEY AUTOINCREMENT, name text, price text, rating text)');
+db.run(
+  "CREATE TABLE categories(name text unique, id INTEGER PRIMARY KEY AUTOINCREMENT)"
+);
+db.run(
+  "CREATE TABLE books(ID INTEGER PRIMARY KEY AUTOINCREMENT, name text, price text, rating text, categoryId INTEGER, FOREIGN KEY(categoryId) REFERENCES categories(id))"
+);
 
 db.close();
